e7e71ca2c7ce91cb56fa7f9195ffa0ab39338dc5
gzenner <gzenner@student.42luxembourg.lu>
2025-10-28 11:08:23 +0100

    images and docs

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ allow the user to list all files of type 'x'
 
 from pathlib import Path
 
-def list_filetype(rootfolder = ".", *ty):
+def list_filetype(rootfolder, gtype, *ty):
+    images = ['*.jpg', '*.png']
+    docs = ['*.odt', '*.doc', '*.md']
+    if gtype == 'images': ty = images
+    if gtype == 'docs': ty = docs
     root = Path(rootfolder)
     for t in ty:
         files = root.rglob(t)
         for file in files: print(file.resolve())
 
-list_filetype('.', '*.py', '*.c', '*.md')
-list_filetype('testfiles', '*.py', '*.c', '*.md')
+list_filetype('.', 'images')
+print('='*30)
+list_filetype('.', 'docs', '*.py', '*.c', '*.md')
+print('='*30)
+#list_filetype('testfiles', 'all', '*.py', '*.c', '*.md')
+print('='*30)
